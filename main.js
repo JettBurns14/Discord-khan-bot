15946c959f8fdbd6a599e48f020e0646165e009f
Ethan Luis McDonough <elmcdonough@comcast.net>
2017-12-05 21:04:30 -0600

    Update package.json

--- main.js
+++ main.js
@@ -623,4 +623,6 @@ client.on('message', message => {
     }*/
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN)
+    .then(() => {})
+    .catch(() => console.log("Invalid token"));
